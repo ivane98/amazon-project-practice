15f0d91a3746ab816dc6452a0dd6f296219c6053
ivane98 <i.vardoshvili22@gmail.com>
2025-01-19 09:17:55 +0400

    'radio'

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatMoney } from "./utils/money.js";
 
 let productHtml = ``;
 products.forEach((product) => {
@@ -26,9 +27,7 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">$${(product.priceCents / 100).toFixed(
-            2
-          )}</div>
+          <div class="product-price">$${formatMoney(product.priceCents)}</div>
 
           <div class="product-quantity-container">
             <select class = "js-quantity-selector-${product.id}">
